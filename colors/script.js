dcea813ed03d75e1c17b8e929d7dc8970032a8ca
Avogado <avogado.gc@gmail.com>
2019-10-04 07:48:16 +0200

    fix

--- colors/script.js
+++ colors/script.js
@@ -1,4 +1,4 @@
 var date = new Date();
 var m = date.getMonth() + 1;
 var d = date.getDate();
-document.write("<style>@media screen { tr:nth-of-type("+m+") td:nth-of-type("+(d+1)+"){ background-color: #ada; } }</style>")
+document.write("<style>@media screen { tr:nth-of-type("+(m+1)+") td:nth-of-type("+(d+1)+"){ background-color: #ada; } }</style>")
